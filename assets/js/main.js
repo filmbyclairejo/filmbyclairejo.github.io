ef756d130662542ce0db61525ce3a4f2b31f8317
adamsebetich <adamsebetich@yahoo.com>
2026-05-27 12:44:42 -0700

    website updates

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,11 @@
-jQuery(window).on('load', function() {   
-    
-    // HIDE PRELAODER
+// HIDE PRELOADER as soon as the DOM is ready, with a safety timeout
+// in case heavy assets (videos/images) delay the window load event.
+jQuery(function () {
     $(".preloader").addClass("hide-preloader");
-  
 });
+setTimeout(function () {
+    if (window.jQuery) { jQuery(".preloader").addClass("hide-preloader"); }
+}, 2000);
 
 
 jQuery(document).ready(function($) {
